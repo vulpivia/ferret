42d5c107eeedb3f5d72312d412587ada4eca890a
Acid147 <norman.rauschen@web.de>
2019-06-22 00:19:17 +0200

    Add linker script to loader

--- ferret.py
+++ ferret.py
@@ -33,16 +33,29 @@ The available commands are:
 		getattr(self, args.command)()
 
 	def build(self):
-		loader_dir_out         = mod_loader + "out"
+		loader_dir_bin         = mod_loader + "bin/"
+		loader_dir_out         = mod_loader + "out/"
+		loader_file_loader_asm = loader_dir_out + "loader.asm"
+		loader_file_loader_ld  = loader_dir_out + "loader.ld"
 		loader_file_loader_lit = mod_loader + "loader.lit"
+		loader_file_loader_o   = loader_dir_out + "loader.o"
 
 		if not os.path.isdir(loader_dir_out):
 			os.mkdir(loader_dir_out)
+		if not os.path.isdir(loader_dir_bin):
+			os.mkdir(loader_dir_bin)
 
+		# Compile Literate to HTML + source code
 		os.system("lit --out-dir " + loader_dir_out + " " + loader_file_loader_lit)
+		# Assemble source code
+		os.system("nasm -felf32 " + loader_file_loader_asm + " -o " + loader_file_loader_o)
+		# Link to binary
+		os.system("ld " + loader_file_loader_ld + " -o " + loader_dir_bin + "loader " + loader_file_loader_asm)
 		exit(0)
 
 	def run(self):
+		# TODO: loader.o is object file, needs ld
+		os.system("qemu-system-i386 -kernel modules/loader/bin/loader.o")
 		exit(0)
 
 if __name__ == "__main__":
